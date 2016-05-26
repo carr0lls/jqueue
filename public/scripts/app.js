5c95899d41002d79d025cafa025c34af003f4d36
Carroll Yu <cyu@commonsense.org>
2016-05-25 19:33:50 -0700

    update UI

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,11 @@ var renderJobs = function() {
 		url: 'api/jobs',
 		method: 'GET',
 		success: function(jobs) {
-			console.log(jobs);
+			// console.log(jobs);
 			jobs.map(function(job) {
+				var refresh_link = '<button onclick="updateJob(this)" class="update" value="'+job._id+'">Refresh</button>';
 				var del_link = '<button onclick="deleteJob(this)" class="delete" value="'+job._id+'">Delete</button>';
-				var link = '<li><a href="api/jobs/'+job._id+'">'+job._id+'</a>'+del_link+'</li>';
+				var link = '<li><a href="api/jobs/'+job._id+'">'+job.url+'</a>'+refresh_link+del_link+'</li>';
 				$('ul.job-list').append(link);
 			});
 		}
@@ -19,11 +20,25 @@ var deleteJob = function(job) {
 		url: 'api/jobs/'+job_id,
 		method: 'DELETE',
 		success: function(res) {
-			console.log(res);
+			// console.log(res);
 			$(job).parent().remove();
 		}
 	});
-}
+};
+
+var updateJob = function(job) {
+	var job_id = $(job).val();
+	$.ajax({
+		url: 'api/jobs/'+job_id,
+		method: 'PUT',
+		success: function(job) {
+			// console.log(job);
+			if (job.error) {
+				alert(job.reason);
+			}
+		}
+	});
+};
 
 $(document).ready(function() {
 
@@ -38,13 +53,14 @@ $(document).ready(function() {
 				method: 'POST',
 				data: data,
 				success: function(job) {
-					console.log(job);
+					// console.log(job);
 					if (job.error) {
 						alert(job.reason);
 					}
 					else {
+						var refresh_link = '<button onclick="updateJob(this)" class="update" value="'+job._id+'">Refresh</button>';
 						var del_link = '<button onclick="deleteJob(this)" class="delete" value="'+job._id+'">Delete</button>';
-						var link = '<li><a href="api/jobs/'+job._id+'">'+job._id+'</a>'+del_link+'</li>';
+						var link = '<li><a href="api/jobs/'+job._id+'">'+job.url+'</a>'+refresh_link+del_link+'</li>';
 						$('ul.job-list').append(link);
 						$('input.url').val('');
 					}
@@ -56,34 +72,12 @@ $(document).ready(function() {
 		}
 	});
 
-	$('.update').on('click', function() {
-		var job_id = $('input.job_id').val();
-		if (job_id) {
-			$.ajax({
-				url: 'api/jobs/'+job_id,
-				method: 'PUT',
-				success: function(job) {
-					console.log(job);
-					if (job.error) {
-						alert(job.reason);
-					}
-					else {
-						$('input.job_id').val('');
-					}
-				}
-			});
-		}
-		else {
-			alert('Type in a job id to update.')
-		}
-	});
-
 	$('.delete-all').on('click', function() {
 		$.ajax({
 			url: 'api/jobs',
 			method: 'DELETE',
 			success: function(res) {
-				console.log(res);
+				// console.log(res);
 				$('ul.job-list').empty();
 			}
 		});
