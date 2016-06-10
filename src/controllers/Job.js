72f5488c6c649b25c743c6fe031bf0a057d916c5
Carroll Yu <cyu@commonsense.org>
2016-06-10 02:15:23 -0700

    integrate real queue system into fetch process

--- src/controllers/Job.js
+++ src/controllers/Job.js
@@ -1,8 +1,24 @@
 import request from 'superagent'
+import rsmq from 'rsmq'
 import * as db from '../models'
-import { getTimeString } from '../helpers'
+
+const jobQueue = new rsmq({host: '127.0.0.1', port: 6379, ns: 'rsmq'})
+const qname = 'jobQueue'
+
+jobQueue.createQueue({qname}, (err, resp) => {
+  if (resp === 1) {
+    console.log("jobQueue created")
+  }
+});
 
 const index = (req, res) => {
+  // jobQueue.getQueueAttributes({qname}, (err, resp) => {
+  //   if (err) {
+  //     res.send(err, 500);
+  //     return;
+  //   }
+  //   res.json(resp)
+  // })
   db.Job.find({}, (err, foundJobs) => {
     if (err) {
       res.json({error: true, reason: err})
@@ -16,7 +32,7 @@ const index = (req, res) => {
 const show = (req, res) => {
   db.Job.findById(req.params.job_id, (err, foundJob) => {
     if (foundJob) {
-      res.json({url: foundJob.url, date: foundJob.date, content: foundJob.content})
+      res.json({status: foundJob.status, url: foundJob.url, date: foundJob.date, content: foundJob.content})
     }
     else {
       res.json({error: true, reason: "Failed to retrieve job."})
@@ -26,74 +42,109 @@ const show = (req, res) => {
 
 const create = (req, res) => {
   if (req.body.url) {
-    request
-      .get(req.body.url)
-      .end((err, result) => {
-        if (err) {
-          let errorMsg = err.reason ? err.reason : err.status
-          if (!errorMsg)
-            errorMsg = 'The url specified is invalid.'
+    let data = {
+      qname,
+      message: req.body.url
+    }
 
-          res.json({error: true, reason: errorMsg})
+    jobQueue.sendMessage(data, (err, qid) => {
+      if (err) {
+        res.send(err, 500);
+        return;
+      }
+
+      let newJob = new db.Job ({
+        qid,
+        url: req.body.url
+      })
+      newJob.save((err, job) => {
+        if (err) {
+          res.json({error: true, reason: "Failed to create new job."})
         }
         else {
-          let newJob = new db.Job ({
-            url: req.body.url,
-            date: getTimeString(),
-            content: result.text
-          })
-
-          newJob.save((err, job) => {
-            if (err) {
-              res.json({error: true, reason: "Failed to create new job."})
-            }
-            else {
-              res.json({_id: job.id, url: job.url})
-            }
-          })
+          res.json({job_id: job.id, url: job.url})
         }
-    })
+      })
+    });
   }
   else {
-    res.json({ error: true, reason: "Fetch url is required."})
+    res.json({error: true, reason: "Fetch url is required."})
   }
+
+  // if (req.body.url) {
+  //   request
+  //     .get(req.body.url)
+  //     .end((err, result) => {
+  //       if (err) {
+  //         let errorMsg = err.reason ? err.reason : err.status
+  //         if (!errorMsg)
+  //           errorMsg = 'The url specified is invalid.'
+  //
+  //         res.json({error: true, reason: errorMsg})
+  //       }
+  //       else {
+  //         let newJob = new db.Job ({
+  //           url: req.body.url,
+  //           date: getTimeString(),
+  //           content: result.text
+  //         })
+  //
+  //         newJob.save((err, job) => {
+  //           if (err) {
+  //             res.json({error: true, reason: "Failed to create new job."})
+  //           }
+  //           else {
+  //             res.json({_id: job.id, url: job.url})
+  //           }
+  //         })
+  //       }
+  //   })
+  // }
+  // else {
+  //   res.json({ error: true, reason: "Fetch url is required."})
+  // }
 }
 
 const update = (req, res) => {
-  db.Job.findById(req.params.job_id, (err, foundJob) => {
-    if (foundJob) {
-      let requestUrl = req.body.url ? req.body.url : foundJob.url
-      request
-        .get(requestUrl)
-        .end((err, result) => {
-          if (err) {
-            let errorMsg = err.reason ? err.reason : err.code
-            res.json({error: true, reason: errorMsg})
-          }
-          else {
-            foundJob.url = requestUrl
-            foundJob.date = getTimeString()
-            foundJob.content = result.text
-            foundJob.save((err, updatedJob) => {
-              if (err) {
-                res.json({error: true, reason: "Failed to update job."})
-              }
-              else {
-                res.json(updatedJob)
-              }
-            })
-          }
-        })
-    }
-    else {
-      res.json({error: true, reason: "Failed to find job to update."})
-    }
-  })
+  // db.Job.findById(req.params.job_id, (err, foundJob) => {
+  //   if (foundJob) {
+  //     let requestUrl = req.body.url ? req.body.url : foundJob.url
+  //     request
+  //       .get(requestUrl)
+  //       .end((err, result) => {
+  //         if (err) {
+  //           let errorMsg = err.reason ? err.reason : err.code
+  //           res.json({error: true, reason: errorMsg})
+  //         }
+  //         else {
+  //           foundJob.url = requestUrl
+  //           foundJob.date = getTimeString()
+  //           foundJob.content = result.text
+  //           foundJob.save((err, updatedJob) => {
+  //             if (err) {
+  //               res.json({error: true, reason: "Failed to update job."})
+  //             }
+  //             else {
+  //               res.json(updatedJob)
+  //             }
+  //           })
+  //         }
+  //       })
+  //   }
+  //   else {
+  //     res.json({error: true, reason: "Failed to find job to update."})
+  //   }
+  // })
 }
 
 const destroy = (req, res) => {
   db.Job.findByIdAndRemove(req.params.job_id, (err, removedJob) => {
     if (removedJob) {
+      jobQueue.deleteMessage({qname, id: removedJob.qid}, (err, resp) => {
+        if (resp === 1) {
+          console.log("message deleted from jobQueue")
+        }
+      })
       res.json({success: true})
     }
     else {
@@ -103,6 +154,11 @@ const destroy = (req, res) => {
 }
 
 const empty = (req, res) => {
+  jobQueue.deleteQueue({qname}, (err, resp) => {
+    if (resp === 1) {
+      console.log("jobQueue deleted")
+    }
+  })
   db.Job.remove({}, (err, removedJobs) => {
     if (err) {
       res.json({error: true, reason: err})
--- src/models/job.js
+++ src/models/job.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import { getTimeString } from '../helpers'
 const Schema = mongoose.Schema
 
 export const JobSchema = new Schema ({
+  qid: String,
   url: String,
-  date: String,
+  date: {type: String, default: getTimeString()},
+  status: {type: String, default: 'PENDING'},
   content: String
 })
 
